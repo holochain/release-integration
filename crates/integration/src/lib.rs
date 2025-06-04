7d4ba49aef85527fd31cf99e697e5aaf0881973e
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-06-04 21:38:46 +0100

    chore: Progress testing

--- crates/integration/src/lib.rs
+++ crates/integration/src/lib.rs
@@ -1,4 +1,5 @@
-use git2::{IndexAddOption, RemoteCallbacks, Repository, RepositoryInitOptions};
+use git2::{IndexAddOption, ObjectType, RemoteCallbacks, Repository, RepositoryInitOptions};
+use std::path::PathBuf;
 use std::sync::OnceLock;
 
 fn git_token() -> String {
@@ -13,6 +14,28 @@ fn git_token() -> String {
         .clone()
 }
 
+pub enum ChangelogConfig {
+    Pre1Point0Cliff,
+}
+
+#[derive(Default)]
+pub enum BumpType {
+    #[default]
+    Auto,
+    Minor,
+}
+
+impl ChangelogConfig {
+    pub fn path(&self) -> PathBuf {
+        let dir = std::env::current_dir().unwrap();
+        match self {
+            ChangelogConfig::Pre1Point0Cliff => {
+                dir.join("../../pre-1.0-cliff.toml").canonicalize().unwrap()
+            }
+        }
+    }
+}
+
 pub struct TestHarness {
     temp_dir: tempfile::TempDir,
     repository: Repository,
@@ -24,9 +47,13 @@ impl TestHarness {
 
         let random_ext = nanoid::nanoid!(5);
         let origin_url = format!("http://localhost:3000/gituser/{project_name}-{random_ext}.git");
+        println!(
+            "Creating repository with origin: {}",
+            temp_dir.path().display()
+        );
         let repository = Repository::init_opts(
             &temp_dir,
-            &RepositoryInitOptions::new()
+            RepositoryInitOptions::new()
                 .origin_url(origin_url.as_str())
                 .initial_head("main"),
         )
@@ -37,6 +64,7 @@ impl TestHarness {
         config
             .set_str("user.email", "gituser@holochain.org")
             .unwrap();
+        config.set_str("credential.helper", "").unwrap();
         let mut index = repository.index().unwrap();
         let tree_id = index.write_tree().unwrap();
         let signature = repository.signature().unwrap();
@@ -45,25 +73,11 @@ impl TestHarness {
                 Some("HEAD"),
                 &signature,
                 &signature,
-                "chore: Init",
+                "chore: init",
                 &repository.find_tree(tree_id).unwrap(),
                 &[],
             )
             .unwrap();
-
-        // Ensure that we aren't using any credential helper in this repository.
-        // We want to authenticate with a username/token instead!
-        let status = std::process::Command::new("git")
-            .current_dir(temp_dir.path())
-            .arg("config")
-            .arg("credential.helper")
-            .arg("")
-            .spawn()
-            .unwrap()
-            .wait()
-            .unwrap();
-        assert!(status.success(), "Failed to set git credential helper");
-
 
         Self {
             temp_dir,
@@ -71,10 +85,24 @@ impl TestHarness {
         }
     }
 
+    pub fn repository_url(&self) -> String {
+        let origin = self
+            .repository
+            .find_remote("origin")
+            .expect("Failed to find remote 'origin'");
+        origin.url().unwrap().to_string()
+    }
+
     pub fn write_file_content(&self, relative_path: &str, content: &str) {
         let file_path = self.temp_dir.path().join(relative_path);
         std::fs::create_dir_all(file_path.parent().unwrap()).unwrap();
         std::fs::write(file_path, content).unwrap();
+    }
+
+    pub fn read_file_content(&self, relative_path: &str) -> String {
+        let file_path = self.temp_dir.path().join(relative_path);
+        std::fs::read_to_string(file_path)
+            .unwrap_or_else(|_| panic!("Failed to read file '{}'", relative_path))
     }
 
     pub fn commit(&self, pattern: &str, message: &str) {
@@ -100,7 +128,17 @@ impl TestHarness {
             .unwrap();
     }
 
-    pub fn push(&self, branch: &str) {
+    pub fn tag(&self, tag: &str, message: &str) {
+        let signature = self.repository.signature().unwrap();
+        let head = self.repository.head().unwrap();
+        let commit = head.peel(ObjectType::Commit).unwrap();
+
+        self.repository
+            .tag(tag, &commit, &signature, message, false)
+            .expect("Failed to create tag");
+    }
+
+    pub fn push_branch(&self, branch: &str) {
         let mut remote = self
             .repository
             .find_remote("origin")
@@ -115,9 +153,144 @@ impl TestHarness {
                 &[format!("refs/heads/{branch}:refs/heads/{branch}")],
                 Some(&mut push_opts),
             )
-            .expect("Failed to push changes to remote");
+            .expect("Failed to push branch to remote");
     }
-    
+
+    pub fn push_tag(&self, tag: &str) {
+        let mut remote = self
+            .repository
+            .find_remote("origin")
+            .expect("Failed to find remote 'origin'");
+
+        let mut push_opts = git2::PushOptions::new();
+        push_opts.remote_callbacks(Self::make_cb());
+
+        remote
+            .push(
+                &[format!("refs/tags/{tag}:refs/tags/{tag}")],
+                Some(&mut push_opts),
+            )
+            .expect("Failed to push tag to remote");
+    }
+
+    pub fn add_crate(&self, crate_model: CrateModel) {
+        self.write_file_content(
+            "Cargo.toml",
+            &format!(
+                r#"[package]
+name = "{}"
+version = "{}"
+edition = "2024"
+{}
+{}
+{}
+        "#,
+                crate_model.name,
+                crate_model.version,
+                if let Some(description) = &crate_model.description {
+                    format!("description = \"{}\"", description)
+                } else {
+                    String::new()
+                },
+                if let Some(repository) = &crate_model.repository {
+                    format!("repository = \"{}\"", repository)
+                } else {
+                    String::new()
+                },
+                if let Some(license) = &crate_model.license {
+                    format!("license = \"{}\"", license)
+                } else {
+                    String::new()
+                },
+            ),
+        );
+
+        let source_path = self.temp_dir.path().join("src");
+        let entry_path = if crate_model.binary {
+            source_path.join("main.rs")
+        } else {
+            source_path.join("lib.rs")
+        };
+
+        if let Some(content) = &crate_model.content {
+            self.write_file_content(entry_path.to_str().unwrap(), content);
+        } else if crate_model.binary {
+            self.write_file_content(
+                entry_path.to_str().unwrap(),
+                "fn main() { println!(\"hello binary\"); }",
+            );
+        } else {
+            self.write_file_content(
+                entry_path.to_str().unwrap(),
+                "// This is a placeholder for the crate content",
+            );
+        }
+    }
+
+    pub fn generate_changelog(
+        &self,
+        changelog_config: ChangelogConfig,
+        bump_type: BumpType,
+    ) -> String {
+        let configure_command = |command: &mut std::process::Command| {
+            command
+                .current_dir(self.temp_dir.path())
+                .arg("--config")
+                .arg(changelog_config.path())
+                .arg("--use-branch-tags");
+
+            match bump_type {
+                BumpType::Auto => command.arg("--bump"),
+                BumpType::Minor => command.arg("--bumpminor").arg("minor"),
+            };
+        };
+
+        let mut command = std::process::Command::new("git-cliff");
+        configure_command(&mut command);
+
+        let exit_status = command
+            .arg("--output")
+            .stderr(std::process::Stdio::inherit())
+            .stdout(std::process::Stdio::inherit())
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+
+        assert!(exit_status.success(), "git-cliff command failed");
+
+        let mut command = std::process::Command::new("git-cliff");
+        configure_command(&mut command);
+
+        let output = command
+            .arg("--context")
+            .stderr(std::process::Stdio::inherit())
+            .stdout(std::process::Stdio::piped())
+            .spawn()
+            .unwrap()
+            .wait_with_output()
+            .unwrap();
+
+        let value = serde_json::from_slice::<serde_json::Value>(&output.stdout).unwrap();
+
+        value
+            .as_array()
+            .unwrap()
+            .first()
+            .unwrap()
+            .as_object()
+            .unwrap()
+            .get("version")
+            .unwrap()
+            .as_str()
+            .unwrap()
+            .to_string()
+    }
+
+    /// Retain the temporary directory and print its path.
+    ///
+    /// Useful for debugging the state of the repository after tests. Alternatively, you can see
+    /// the state of the repository on Gitea.
     pub fn retain(self) {
         let path = self.temp_dir.keep();
         println!("Repository retained at: {}", path.display());
@@ -135,5 +308,54 @@ impl TestHarness {
             Ok(created)
         });
         cb
+    }
+}
+
+pub struct CrateModel {
+    pub name: String,
+    pub version: String,
+    pub binary: bool,
+    pub description: Option<String>,
+    pub repository: Option<String>,
+    pub license: Option<String>,
+    pub content: Option<String>,
+}
+
+impl CrateModel {
+    pub fn new(name: &str, version: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            version: version.to_string(),
+            binary: true,
+            description: None,
+            repository: None,
+            license: None,
+            content: None,
+        }
+    }
+
+    pub fn make_lib(mut self) -> Self {
+        self.binary = false;
+        self
+    }
+
+    pub fn with_description(mut self, description: &str) -> Self {
+        self.description = Some(description.to_string());
+        self
+    }
+
+    pub fn with_repository(mut self, repository: &str) -> Self {
+        self.repository = Some(repository.to_string());
+        self
+    }
+
+    pub fn with_license(mut self, license: &str) -> Self {
+        self.license = Some(license.to_string());
+        self
+    }
+
+    pub fn with_content(mut self, content: &str) -> Self {
+        self.content = Some(content.to_string());
+        self
     }
 }
--- crates/integration/tests/changelog.rs
+++ crates/integration/tests/changelog.rs
@@ -1,12 +1,114 @@
-use integration::TestHarness;
+use integration::{BumpType, ChangelogConfig, CrateModel, TestHarness};
 
+/// A really simple library crate to check that changelog generation behaves as expected.
+///
+/// With this test, we get:
+/// - Semver is respected for `chore:` and `feat:` commits when the crate is using a 0.x.y version.
 #[test]
-fn first() {
-    let harness = TestHarness::new("first");
+fn simple_library_changelog() {
+    let harness = TestHarness::new("library");
 
+    //
+    // Initialize the repository
+    //
     harness.write_file_content("README.md", "# hello world");
     harness.commit("README.md", "chore: Add README");
-    harness.push("main");
+    harness.push_branch("main");
+
+    //
+    // Add Rust source code
+    //
+    let new_crate = CrateModel::new("test", "0.1.0")
+        .make_lib()
+        .with_description("A test crate")
+        .with_repository(harness.repository_url().as_str())
+        .with_license("Apache-2.0");
+
+    harness.add_crate(new_crate);
+    harness.commit("*", "chore: Add crate");
+    harness.push_branch("main");
+
+    //
+    // Generate the initial changelog
+    //
+    let version = harness.generate_changelog(ChangelogConfig::Pre1Point0Cliff, BumpType::default());
+    assert_eq!(version, "v0.1.0");
+
+    // Check the changelog content
+    let changelog = harness.read_file_content("CHANGELOG.md");
+    assert!(changelog.contains("## [0.1.0]"));
+    assert!(changelog.contains("### Changed"));
+    assert!(changelog.contains("Add crate"));
+
+    //
+    // Push a tag for the version
+    //
+    harness.commit("*", "docs: Update changelog for v0.1.0");
+    harness.tag(version.as_str(), version.as_str());
+    harness.push_branch("main");
+    harness.push_tag(version.as_str());
+
+    //
+    // Make a simple change to the library
+    //
+    harness.write_file_content("src/lib.rs", "fn add(a: i32, b: i32) -> i32 { a + b }");
+    harness.commit("src/lib.rs", "chore: Add add function");
+    harness.push_branch("main");
+
+    //
+    // Generate the changelog for the new version
+    //
+    let version = harness.generate_changelog(ChangelogConfig::Pre1Point0Cliff, BumpType::default());
+    assert_eq!(version, "v0.1.1");
+
+    // Check the changelog content
+    let changelog = harness.read_file_content("CHANGELOG.md");
+    assert!(changelog.contains("## [0.1.0]"));
+    assert!(changelog.contains("## [0.1.1]"));
+    assert!(changelog.contains("### Changed"));
+    assert!(changelog.contains("Add add function"));
+
+    //
+    // Push a tag for the new version
+    //
+    harness.commit("*", "docs: Update changelog for v0.1.1");
+    harness.tag(version.as_str(), version.as_str());
+    harness.push_branch("main");
+    harness.push_tag(version.as_str());
+
+    //
+    // Make another change to the library and this time call it a feature
+    //
+    harness.write_file_content(
+        "src/lib.rs",
+        r#"fn add(a: i32, b: i32) -> i32 { a + b }
+fn subtract(a: i32, b: i32) -> i32 { a - b }
+"#,
+    );
+    harness.commit("src/lib.rs", "feat: Add subtract function");
+    harness.push_branch("main");
+
+    //
+    // Generate the changelog for the new version
+    //
+    let version = harness.generate_changelog(ChangelogConfig::Pre1Point0Cliff, BumpType::default());
+    assert_eq!(version, "v0.1.2");
+
+    // Check the changelog content
+    let changelog = harness.read_file_content("CHANGELOG.md");
+    assert!(changelog.contains("## [0.1.0]"));
+    assert!(changelog.contains("## [0.1.1]"));
+    assert!(changelog.contains("## [0.1.2]"));
+    assert!(changelog.contains("### Changed"));
+    assert!(changelog.contains("Add subtract function"));
+
+    //
+    // Push a tag for the new version
+    //
+    harness.commit("*", "docs: Update changelog for v0.1.2");
+    harness.tag(version.as_str(), version.as_str());
+    harness.push_branch("main");
+    harness.push_tag(version.as_str());
 
     harness.retain();
 }
